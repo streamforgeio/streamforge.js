a240224ee5c18587a7197e329d026b0b340aab55
ilker.ernas <ilker.ernas@verapi.com>
2018-12-09 13:27:44 +0100

    inheritance is applied

--- pipeline.js
+++ pipeline.js
@@ -1,80 +1,95 @@
 const fs = require('fs');
 const winston = require('winston')
 
+function PipelineObject(){
+    this.components = new Array();
+}
+PipelineObject.prototype.withComponent = function(z){
+    this.components.push(z);
+    return this;
+}
+PipelineObject.prototype.save = function(path){
+    fs.writeFile(path, JSON.stringify(this), function(err) {
+        if(err) {
+            return winston.log('error','Error while saving to ' + path);
+        }
+        winston.log('info',"The file was saved!");
+    });             
+},
+PipelineObject.prototype.submit = function(){
+    // submit to streamforge.io
+}
 
 function Pipeline(){
-    const components = new Array();
-    return {
-        withComponent: function(z){
-            components.push(z.jsonObject());
-            return this;
-        },
-        jsonObject: function(){
-            return {
-                "components" : components
-            }
-        },
-        save: function(path){
-            fs.writeFile(path, JSON.stringify(this.jsonObject()), function(err) {
-                if(err) {
-                    return winston.log('error','Error while saving to ' + path);
-                }
-                winston.log('info',"The file was saved!");
-            });             
-        },
-        submit : function(){
-            // submit to streamforge.io
-        }
+    return new PipelineObject();
+}
 
-    }
+function PipelineComponent(aliasParam){
+    this.alias = aliasParam;
+    this.conflation = "";
+}
+PipelineComponent.prototype.withConflation = function(conflateFunc){
+    this.conflation = conflateFunc.toString();
+    return this;
+}
 
+function SourceObject(aliasParam,criteriaParam,maskParam){
+    PipelineComponent.call(this,aliasParam);
+    this.criteria = criteriaParam;
+    this.mask = maskParam;
+    this.process = ""
+}
+SourceObject.prototype.withProcess = function(process){
+    this.process = process.toString();
+    return this;
+}
+/*SourceObject.prototype.toJSON = function(){
+    return JSON.stringify({
+        "alias" : this.alias,
+        "mask" : this.mask,
+        "criteria" : this.criteria,
+        "process" : 
+    });
+}*/
+SourceObject.prototype = Object.create(PipelineComponent.prototype);
 
+function Source(alias,criteria,mask){
+   return new SourceObject(alias,criteria,mask);
+}
+
+function Criteria(field, operator, value) {
+    this.field = field;
+    this.operator = operator;
+    this.value = value;
+}
+
+function PipelineProcessingComponent(aliasParam){
+    PipelineComponent.call(this,aliasParam);
+    this.process = function(){};
+}
+PipelineProcessingComponent.prototype = Object.create(PipelineComponent.prototype);
+PipelineProcessingComponent.prototype.withProcess = function(func){
+    process = func;
+    return this;
+}
+
+function ZipObject(aliasParam){
+    PipelineProcessingComponent.call(this,aliasParam);
+    this.sources = new Array();
+}
+ZipObject.prototype = Object.create(PipelineProcessingComponent.prototype);
+
+ZipObject.prototype.withSource = function(source){
+    this.sources.push(source);
+    return this;
 }
 
 function Zip(aliasParam){
-    const alias = aliasParam;
-    var process = "";
-    const sources = new Array();
-    return {
-        withSource: function(source){
-            sources.push(source);
-            return this;
-        },
-        withProcess: function(func){
-            process = func;
-            return this;
-        },
-        jsonObject: function(){
-            return {
-                "alias" : alias,
-                "process" : process.toString(),
-                "sources" : sources
-            }
-        }
-
-    }
-
-
+    return new ZipObject(aliasParam);
 }
 
 
-function Source(alias,criteria,mask){
-    return {
-        "alias" :  alias,
-        "criteria" : criteria,
-        "mask" : mask
-    }
-}
-
-
-function Criteria(field, operator, value) {
-    return {
-        "field" : field,
-        "operator" : operator,
-        "value" : value
-    }
-}
-
+SourceObject.prototype = Object.create(PipelineComponent.prototype);
 
 
 module.exports = {Pipeline,Zip,Source,Criteria}
--- runny.js
+++ runny.js
@@ -1,5 +1,9 @@
 const sf = require('./pipeline.js');
 
 
-var p = sf.Pipeline().withComponent(sf.Zip("z1").withSource(sf.Source("falan")))
-p.save("/tmp/falan2.json")
+//var z = sf.Source("aa11",null,null).withConflation(function(){console.log("asdsad")});
+
+
+var p = sf.Pipeline().withComponent(sf.Zip("z1").withProcess(function(){}).withSource(sf.Source("falan")))
+console.log(JSON.stringify(p));
+p.save("/tmp/sf.json")
